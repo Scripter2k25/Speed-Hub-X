23816e651e52fd3072c44637e369d526212efaf5
No-Lag-PH <182519863+Scripter2k25@users.noreply.github.com>
2025-08-12 04:12:27 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/zgrSToMv/raw"))()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripter2k25/Speed-Hub-XV2/refs/heads/main/GameList.lua"))()
